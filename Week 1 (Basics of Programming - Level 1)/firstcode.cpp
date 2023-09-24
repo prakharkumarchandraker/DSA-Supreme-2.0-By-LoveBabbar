43493b8d0e5d5187282140911899f3a1fabcd27b
Prakhar Kumar Chandraker <prakharkumarchandraker@gmail.com>
2023-09-24 20:49:17 +0530

    Commit

---
+++ Week 1 (Basics of Programming - Level 1)/firstcode.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Prakhar" << "\n" << "Prakhar" << endl << "Prakhar";
+    // cout << "Prakhar" << "\n" << "Prakhar" << endl << "Prakhar";
+    return 0;
+}
